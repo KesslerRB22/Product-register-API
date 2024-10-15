f5ee50ecf1b8b2755c8ed8afbfed1fca1ccd0c8b
KesslerBarreto <kesslergremio@gmail.com>
2024-10-15 14:21:34 -0300

    First version product register API

--- nome-caixa-alta-arrow-function.js
+++
@@ -1,7 +0,0 @@
-const converterParaMaiusculas = (nome) => {
-    console.log(nome.toUpperCase());
-};
- 
-let nome = "Kessler";
-
-converterParaMaiusculas(nome);
--- verificador-de-idade-arrow-function.js
+++
@@ -1,6 +0,0 @@
-let idade = 19;
- 
-const maior = (idade) => idade >= 18 ? "Maior de idade" : "Menor de idade";
- 
-console.log(maior(idade));
-
